9c6e6ff71ebfe8d1b1fc731f7e2286a9bf6cc578
Terry Babyuk <tbabyuk@gmail.com>
2023-06-18 02:18:20 -0400

    reverted back to previous day style, iOS still not displaying as it should

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -73,7 +73,7 @@ export const Calendar = () => {
       footer = <p>You picked {selectedDate}.</p>;
     }
 
-    const bookedStyle = {border: "2px solid orange", background: "blue"}
+    const bookedStyle = {border: "2px solid orange"}
     const selectedStyle = {background: "orange"}
 
     const closeEditTaskModal = () => {
